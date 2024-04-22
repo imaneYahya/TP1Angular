e564a7e68e878582e1dd717340b32c0346a2b8e0
Imane YAHYA <imaneyahya86@gmail.com>
2024-04-22 23:10:15 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { NewProductComponent } from './new-product/new-product.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductsComponent,
-    NewProductComponent
+    NewProductComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,19 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {update} from "@angular-devkit/build-angular/src/tools/esbuild/angular/compilation/parallel-worker";
+import {ProductService} from "../services/product.service";
+import {Product} from "../model/product.model";
+import {Observable} from "rxjs";
+import {log} from "util";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-  products : Array<any>=[
-    {id : 1, name :  "computer" , price : 40559 , checked : false},
-    {id : 2, name :  "appleWatch" , price : 2000 , checked : true},
-    {id : 3, name :  "phone" ,  price : 4559 , checked : false},
-  ]
+export class ProductsComponent implements OnInit{
+   public products : Array<Product> =[];
+  public keyword : String="";
+  constructor(private productService : ProductService) {
+  }
+  ngOnInit(){
+  this.getProducts();
+  }
+  getProducts(){
 
-  handleCheckProduct(product : any) {
-    product.checked = !product.checked;
+    this.productService.getProducts()
+      .subscribe({
+        next : data =>
+        {this.products = data},
+        error : err => {
+          console.log(err);
+        }
+      })
+
+
+  }
+
+  handleCheckProduct(product : Product) {
+
+    this.productService.checkProduct(product)
+      .subscribe({
+
+     next:updateProduct =>{
+        product.checked = !product.checked;
+        //this.getProducts();
+      }
+    })
+
+
+
+  }
+
+  handelDelete(product : Product) {
+    if(confirm("are u sure"))
+    this.productService.deleteProduct(product)
+      .subscribe({
+        next : value =>{
+         // this.getProducts();
+       this.products =   this.products.filter(p=>p.id!= product.id);
+    },
+        error: err => {
+          console.log('Error deleting product:', err);
+        }
+      })
+
+  }
+
+  searchProduct() {
 
   }
 }
